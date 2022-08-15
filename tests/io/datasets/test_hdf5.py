094333335f62f161a3620996932dc0e8dad8f339
Dieter Weber <d.weber@fz-juelich.de>
2022-08-15 15:58:52 +0200

    DataTile as a container class
    ...not as a subclass of an array type.
    
    This avoids several calls to the array constructor, which brings a
    noticeable improvement in the inner processing loop for sparse.pydata.org arrays.
    
    Since the tile is unwrapped and the underlying array is passed to the
    UDF now, access to the scheme_idx and tile_slice attributes of a tile
    is not possible anymore. They are now available under UDFMeta. An
    error message points the user to the new alternative.
    
    Since this was a "half-internal" interface, it is trivial to migrate
    and is only useful for advanced UDFs, it is not deprecated but removed
    hard.

--- tests/io/datasets/test_hdf5.py
+++ tests/io/datasets/test_hdf5.py
@@ -649,7 +649,7 @@ def test_hdf5_macrotile_roi(lt_ctx, hdf5_ds_1):
     p0 = next(partitions)
     m0 = p0.get_macrotile(roi=roi)
     assert_allclose(
-        m0,
+        m0.data,
         expected
     )
 
@@ -661,7 +661,7 @@ def test_hdf5_macrotile_empty_roi(lt_ctx, hdf5_ds_1):
     m0 = p0.get_macrotile(roi=roi)
     assert m0.shape == (0, 16, 16)
     assert_allclose(
-        m0,
+        m0.data,
         0,
     )
 
