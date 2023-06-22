3751b867e834cab3f82d9cdde3245e526c31de12
Alexander Clausen <alex@gc-web.de>
2023-06-22 10:57:36 +0200

    Don't crash on acquisition cancellation

--- src/libertem/exceptions.py
+++ src/libertem/exceptions.py
@@ -3,3 +3,11 @@ class UDFException(Exception):
     Raised when the UDF interface is somehow misused
     """
     pass
+
+
+class AcquisitionCancelled(Exception):
+    """
+    Raised when the data stream received is cancelled before the expected size
+    was reached.
+    """
+    pass
